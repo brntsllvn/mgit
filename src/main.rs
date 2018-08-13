821e809e34e6a268031377a6cdcfc847c9b51ff4
Brent Sullivan <brntsllvn@gmail.com>
2018-08-12 19:46:18 -0700

    add default text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let default_output = vec!(
+        "command        Description".to_string(),
+        "----------------------------------------------".to_string(),
+        "init           Create an empty Git repository".to_string(),
+        "add            Add file contents to the index".to_string(),
+        "commit         Record changes to the repository".to_string()
+    );
+
+    println!("{}", '\n');
+    for item in default_output.iter() {
+        println!("{}", item);
+    }
+    println!("{}", '\n');
 }
