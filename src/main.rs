34688612426edf555b2d61f1b1d076177c72caac
Brent Sullivan <brntsllvn@gmail.com>
2018-08-12 22:28:29 -0700

    add pattern match for command line args

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
-fn main() {
-    let default_output = vec!(
-        "command        Description".to_string(),
-        "----------------------------------------------".to_string(),
-        "init           Create an empty Git repository".to_string(),
-        "add            Add file contents to the index".to_string(),
-        "commit         Record changes to the repository".to_string()
-    );
+use std::env;
+use std::process;
 
-    println!("{}", '\n');
-    for item in default_output.iter() {
-        println!("{}", item);
-    }
-    println!("{}", '\n');
+
+fn main() {
+    let args = env::args();
+
+    match args.skip(1).next() {
+        Some(command) => match command.as_ref() {
+            "init" => println!("running init..."),
+            "add" => println!("running add..."),
+            "commit" => println!("running commit..."),
+            _ => println!("something went wrong: unrecognized command")
+        },
+        None => {
+            println!("printing default stuff");
+            process::exit(0);
+        }
+    };
 }
+
+
+//    let default_output = vec!(
+//        "command        Description".to_string(),
+//        "----------------------------------------------".to_string(),
+//        "init           Create an empty Git repository".to_string(),
+//        "add            Add file contents to the index".to_string(),
+//        "commit         Record changes to the repository".to_string()
+//    );
+
+
+
+
+// accept command line args
+// determine which struct (default, init, add, commit) to use (ideally make the return type an abstraction)
+// create the struct
