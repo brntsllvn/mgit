63c1b7fa69a544513996a0d56814e315ae7c3ad6
Brent Sullivan <brntsllvn@gmail.com>
2018-08-13 22:27:16 -0700

    add missing command behavior

--- src/main.rs
+++ src/main.rs
@@ -16,24 +16,51 @@ init           Create an empty Git repository
 add            Add file contents to the index
 commit         Record changes to the repository
 ";
-
         println!("{}", default);
-
         default.to_string()
     }
 }
 
-// InitCommand.execute...returns "Initialized empty git repo"
-// AddCommand.execute...returns ""
-// CommitCommand.execute...returns "SHA-1"
+pub struct InitCommand;
+impl Command for InitCommand {
+    fn execute(&self) -> String {
+        // implementation goes here
+        let msg = "Initialized empty git repo".to_string();
+        println!("{}", msg);
+        msg
+    }
+}
+
+pub struct AddCommand;
+impl Command for AddCommand {
+    fn execute(&self) -> String {
+        // implementation goes here
+        let msg = "Index updated".to_string();
+        println!("{}", msg);
+        msg
+    }
+}
+
+pub struct CommitCommand;
+impl Command for CommitCommand {
+    fn execute(&self) -> String {
+        // implementation goes here
+        let msg = "Committed...".to_string();
+        println!("{}", msg);
+        msg // SHA-1
+    }
+}
 
 fn main() {
     match env::args().skip(1).next() {
         Some(command) => match command.as_ref() {
-            "init" => println!("running init..."),
-            "add" => println!("running add..."),
-            "commit" => println!("running commit..."),
-            _ => println!("something went wrong: unrecognized command")
+            "init" => InitCommand.execute(),
+            "add" => AddCommand.execute(),
+            "commit" => CommitCommand.execute(),
+            _ => {
+                println!("mgit: '{}' is not a git command. See 'mgit' for help", command);
+                process::exit(-1);
+            }
         },
         None => {
             EmptyCommand.execute();
