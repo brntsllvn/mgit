65a80b2cc62b641d2143516f2ce86639933344ae
Brent Sullivan <brntsllvn@gmail.com>
2018-08-18 12:50:52 -0700

    use factory pattern to get command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -6,5 +6,17 @@ pub mod commit;
 pub mod missing;
 
 pub trait Command {
-    fn execute(&self, mut args: env::Args) -> String;
+    fn execute(&self, args: env::Args) -> String;
+}
+
+pub fn get_command(param: Option<String>) -> Box<Command> {
+    match param {
+        Some(txt) => match txt.as_ref() {
+            "init" => Box::new(init::InitCommand) as Box<Command>,
+            "add" => Box::new(add::AddCommand) as Box<Command>,
+            "commit" => Box::new(commit::CommitCommand) as Box<Command>,
+            _ => Box::new(missing::MissingCommand) as Box<Command>
+        },
+        None => Box::new(empty::EmptyCommand) as Box<Command>
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,7 @@ use commands::*;
 fn main() {
     let mut args= env::args();
     let _filepath= args.next();
-    let command= args.next();
-    let result= match command {
-        Some(cmd) => match cmd.as_ref() {
-            "init" => commands::init::InitCommand.execute(args),
-            "add" => commands::add::AddCommand.execute(args),
-            "commit" => commands::commit::CommitCommand.execute(args),
-            _ => commands::missing::MissingCommand.execute(args)
-        },
-        None => commands::empty::EmptyCommand.execute(args)
-    };
-
+    let command = commands::get_command(args.next());
+    let result = command.execute(args);
     println!("\n{}\n", result);
 }
