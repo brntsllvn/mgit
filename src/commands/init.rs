7bbee11cb2d5ce657ec612558825f6a02bbf5080
Brent Sullivan <brntsllvn@gmail.com>
2018-08-18 16:53:40 -0700

    save

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,15 +1,35 @@
 use std::env;
 use commands::Command;
+use std::fs;
 
 pub struct InitCommand;
 
 impl Command for InitCommand {
     fn execute(&self, mut args: env::Args) -> String {
 
-        //////////////////////////////
-        // implementation goes here //
-        //////////////////////////////
+//        fs::create_dir("./.mgit");
+//        fs::create_dir("./.mgit/objects");
+//        fs::create_dir("./.mgit/refs");
 
         "Initialized empty git repo".to_string()
+    }
+}
+
+// ./.git/objects
+// ./.git/refs
+// ./.git
+
+use std::fs::File;
+//use std::io::prelude::*;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_git_database() {
+        let dummy_args: env::Args = Args::
+        InitCommand::execute();
+        let open_result = File::open("./.mgit");
     }
 }
