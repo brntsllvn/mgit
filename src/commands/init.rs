573d020dd4f16e49a362d0d92c5ff24526178252
Brent Sullivan <brntsllvn@gmail.com>
2018-08-18 17:20:07 -0700

    write tests for init command

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,35 +1,63 @@
 use std::env;
 use commands::Command;
 use std::fs;
+use std::fs::File;
+use std::error::Error;
 
 pub struct InitCommand;
 
 impl Command for InitCommand {
     fn execute(&self, args: Vec<String>) -> String {
 
-//        fs::create_dir("./.mgit");
-//        fs::create_dir("./.mgit/objects");
-//        fs::create_dir("./.mgit/refs");
+        fs::create_dir("./.mgit");
+        fs::create_dir("./.mgit/objects");
+        fs::create_dir("./.mgit/refs");
 
         "Initialized empty git repo".to_string()
     }
 }
 
-// ./.git/objects
-// ./.git/refs
-// ./.git
-
-use std::fs::File;
-//use std::io::prelude::*;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn create_git_database() {
-        let dummy_args: env::Args = Args::
-        InitCommand::execute();
-        let open_result = File::open("./.mgit");
+    fn creates_mgit_db() {
+        let dummy_args = vec!["hi".to_string()];
+        let path = "./.mgit";
+        let command = InitCommand.execute(dummy_args);
+        let result = File::open(path);
+        match result {
+            Err(_) => panic!("dir does not exist"),
+            Ok(_) => println!("pass")
+        };
+        fs::remove_dir(path);
+    }
+
+    #[test]
+    fn creates_obj_dir() {
+        let dummy_args = vec!["hi".to_string()];
+        let path = "./.mgit/objects";
+        let command = InitCommand.execute(dummy_args);
+        let result = File::open(path);
+        match result {
+            Err(_) => panic!("dir does not exist"),
+            Ok(_) => println!("pass")
+        };
+        fs::remove_dir(path);
+    }
+
+    #[test]
+    fn creates_refs_dir() {
+        let dummy_args = vec!["hi".to_string()];
+        let path = "./.mgit/refs";
+        let command = InitCommand.execute(dummy_args);
+        let result = File::open(path);
+        match result {
+            Err(_) => panic!("dir does not exist"),
+            Ok(_) => println!("pass")
+        };
+        fs::remove_dir(path);
     }
 }
